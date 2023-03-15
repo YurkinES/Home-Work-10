249d189c8e164a9eb7ea94da86422db792ef3eb0
Yurkines <6eJlomop@inbox.ru>
2023-03-15 20:17:47 +0300

    Start stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
     }
 }
