82c47240270a708e366c7c01eec02a246d550700
Yurkines <6eJlomop@inbox.ru>
2023-03-15 20:23:18 +0300

    First stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+    public static void task1(){
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
