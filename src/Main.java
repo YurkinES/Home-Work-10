ac8d05d749330de1b4b83f5b5695756217f60bd0
Yurkines <6eJlomop@inbox.ru>
2023-03-15 20:33:33 +0300

    Third stage

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1(){
         String firstName = "Ivan";
@@ -19,4 +20,9 @@ public class Main {
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
     }
 
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
+    }
 }
